dd3f603f4f40106c9315dce1d70e5cbeb348a134
Isaac <admin@admins-air.home>
2023-03-02 22:04:23 +0000

    Initialised web pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import{
+  BrowserRouter,
+  Routes,
+  Route,
+}from "react-router-dom";
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Settings from "./pages/Settings";
+import Questions from "./pages/Questions";
+import FinalScreen from "./pages/FinalScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<Settings/>} />
+        <Route path="/questions" element={<Questions/>}/>
+        <Route path="/finalScreen" element={<FinalScreen/>}/>
+      </Routes>
+    </BrowserRouter>
+      
   );
 }
 
